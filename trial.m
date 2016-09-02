e46e6e93dfb1c5926db74921b149e5ec4535555b
Aryan Kashyap <matrixapp13@gmail.com>
2016-09-02 22:10:51 +0100

    Adding Files

---
+++ trial.m
@@ -0,0 +1,160 @@
+function varargout = trial(varargin)
+% TRIAL MATLAB code for trial.fig
+%      TRIAL, by itself, creates a new TRIAL or raises the existing
+%      singleton*.
+%
+%      H = TRIAL returns the handle to a new TRIAL or the handle to
+%      the existing singleton*.
+%
+%      TRIAL('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TRIAL.M with the given input arguments.
+%
+%      TRIAL('Property','Value',...) creates a new TRIAL or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before trial_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to trial_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help trial
+
+% Last Modified by GUIDE v2.5 02-Sep-2016 21:58:56
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @trial_OpeningFcn, ...
+                   'gui_OutputFcn',  @trial_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before trial is made visible.
+function trial_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to trial (see VARARGIN)
+
+% Choose default command line output for trial
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes trial wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = trial_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+%tell that a and b texfield handles a string
+a = get(handles.a, 'string');
+b = get(handles.b, 'string');
+
+% the textfield wants a string but the answer is a number so num2str
+% converts a number to string and str2num converts a string to number.
+c = num2str(str2num(a)+str2num(b))
+
+%set.
+set(handles.c, 'string', c);
+
+
+
+function a_Callback(hObject, eventdata, handles)
+% hObject    handle to a (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of a as text
+%        str2double(get(hObject,'String')) returns contents of a as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function a_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to a (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function b_Callback(hObject, eventdata, handles)
+% hObject    handle to b (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of b as text
+%        str2double(get(hObject,'String')) returns contents of b as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function b_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to b (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function c_Callback(hObject, eventdata, handles)
+% hObject    handle to c (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of c as text
+%        str2double(get(hObject,'String')) returns contents of c as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function c_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to c (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
